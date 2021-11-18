3da69a6a8c6736293054a0ccf549d0ab52e1916c
kevin <kevinks1210@gmail.com>
2021-11-17 19:19:11 -0700

    almost done with doc home page

--- CSE360Project/src/main/java/com/example/cse360project/DocPatientViewLogIn.java
+++ CSE360Project/src/main/java/com/example/cse360project/DocPatientViewLogIn.java
@@ -14,10 +14,11 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class DocPatientViewLogIn {
+public class DocPatientViewLogIn extends DocHomePageController{
 
     public Button enterButton;
     public TextField LastNameField, DOBField;
+
 
 
     public void handleEnterButton(){
@@ -29,13 +30,34 @@ public class DocPatientViewLogIn {
             Scanner searcher = new Scanner(myFile);
             while(searcher.hasNextLine()){
                 String[] array = searcher.nextLine().split(",");
+                if(array[2].equals(lastName)){
+                    if(array[3].equals(bDay)){
+                        Stage closeScene = (Stage) enterButton.getScene().getWindow();
+                        closeScene.close();
 
+                         DocHomePageController.Email = array[0];
+
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("DoctorHomePage.fxml"));
+                        Parent root = loader.load();
+
+                        Stage stage = new Stage();
+                        stage.close();
+                        Scene newScene = new Scene(root);
+                        stage.setScene(newScene);
+                        stage.setTitle("Welcome!");
+                        stage.show();
+
+                        return;
+                    }
+                }
             }
 
         }
         catch(FileNotFoundException exception){
             System.out.println();
             exception.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
